0a16518341f696efcf4a5edf10db49cf4ebd8dac
Arthur <Frenki_art@mail.ru>
2023-09-15 17:35:32 +0300

    feat: add .header & .list-qwests

--- src/main.js
+++ src/main.js
@@ -8,23 +8,26 @@ import './assets/scss/header.scss';
 import './assets/scss/sections.scss';
 import './assets/scss/footer.scss';
 
-import './assets/scss/breadcrumb.scss';
-import './assets/scss/typograf.scss';
+//import './assets/scss/breadcrumb.scss';
+//import './assets/scss/typograf.scss';
 import './assets/scss/links.scss';
 import './assets/scss/buttons.scss';
+
+import './assets/scss/qwest.scss';
+import './assets/scss/address.scss';
 
 import './assets/scss/stuff.scss';
 import './assets/scss/added-styles.scss';
 
-import './assets/scss/shortened.scss';
-import './assets/scss/acc.scss';
-import './assets/scss/pagination.scss';
-import './assets/scss/popup.scss';
+//import './assets/scss/shortened.scss';
+//import './assets/scss/acc.scss';
+//import './assets/scss/pagination.scss';
+//import './assets/scss/popup.scss';
 
-import './assets/js/tabs.js';
-import './assets/js/acc.js';
-import './assets/js/shortened.js';
-import './assets/js/popup.js';
-import './assets/js/youtube.js';
+//import './assets/js/tabs.js';
+//import './assets/js/acc.js';
+//import './assets/js/shortened.js';
+//import './assets/js/popup.js';
+//import './assets/js/youtube.js';
 
 import './assets/js/my.js';
